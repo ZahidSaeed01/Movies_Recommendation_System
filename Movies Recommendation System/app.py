9c7c4dae7cbd51a6a0abdf7cabeeb79e63092b00
Zahid010 <57994542+Zahid010@users.noreply.github.com>
2019-12-06 10:15:42 +0500

    Update app.py

--- Movies Recommendation System/app.py
+++ Movies Recommendation System/app.py
@@ -3,17 +3,18 @@ import pandas as pd
 from imdb import IMDb
 import Knn
 
+ia = IMDb()
+
 movies_dataframe = pd.read_csv('movies.csv')
 links_dataframe = pd.read_csv('links.csv')
 ratings_dataframe = pd.read_csv('ratings.csv')
 tags_dataframe = pd.read_csv('tags.csv')
-ia = IMDb()
+processedDataframe = pd.read_csv('processed_data.csv')
 
 recommender = Knn.KNN(movies_dataframe,links_dataframe,ratings_dataframe,tags_dataframe)
-recommender.extract_imp_movie_data()
-recommender.set_movie_Ids()
-recommender.set_average_ratings()
-#recommender.set_genres()
+
+recommender.processedData = processedDataframe
+
 
 def get_movies(imdb_ids):
   movies_data = []
@@ -26,8 +27,6 @@ def get_movies(imdb_ids):
 
 
 app = Flask(__name__)
-
-
 
 
 @app.route("/")
